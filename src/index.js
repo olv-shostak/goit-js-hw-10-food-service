5d484680ebc3f234c3172c18aea276bbcc15a03a
Oleksandr Shostak <olv.shostak@gmail.com>
2021-10-12 18:23:00 +0300

    Add markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './sass/main.scss';
+import markupTpl from './templates/markup.hbs';
+import menu from './menu.json';
+
+
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+  const paletteContainer = document.querySelector('.js-menu');
+  const cardsMarkup = createMarkup(menu);
+  paletteContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+  function createMarkup(menu) {
+     return  menu.map(markupTpl).join('');
+  }
